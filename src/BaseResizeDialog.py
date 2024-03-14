45086e751059e5586f4eac51631745440a87476b
quniLcs <quniLcs@gmail.com>
2024-03-14 09:40:21 +0800

    Add BaseResizeDialog

---
+++ src/BaseResizeDialog.py
@@ -0,0 +1,42 @@
+# coding =utf-8
+
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.Qt import *
+from src.view.BaseResizeDialog import  Ui_baseResizeDialog
+from functools import partial
+
+
+class BaseResizeDialog(QDialog,Ui_baseResizeDialog):
+
+    dialogRejected = pyqtSignal()
+    dialogAccepted = pyqtSignal(object, object)
+
+    def __init__(self,*args,**kwargs):
+        super(BaseResizeDialog, self).__init__(*args,**kwargs)
+        self.setupUi(self)
+        self._establishConnections()
+
+    def _establishConnections(self):
+        self.dialogBtnBox.accepted.connect(self._dialogAccepted)
+        self.dialogBtnBox.rejected.connect(self._dialogRejected)
+
+    def _dialogAccepted(self):
+        width = self.spinBoxWidth.value()
+        height = self.spinBoxHeight.value()
+        self.dialogAccepted.emit(width, height)
+
+    def _dialogRejected(self):
+        self.dialogRejected.emit()
+
+
+def main():
+    app = QApplication(sys.argv)
+    application = BaseResizeDialog()
+    application.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
